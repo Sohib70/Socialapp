e2ee0f2cde261e773c26616c9646f7052afa2d8d
shaxriyorsharofov <shaxriyorsharofov911@gmail.com>
2025-09-06 14:06:57 +0500

    regis

---
+++ users/models.py
@@ -0,0 +1,112 @@
+import random
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import FileExtensionValidator
+from shared.models import BaseModel
+from datetime import datetime, timedelta
+import uuid
+# Create your models here.
+
+ORDINARY_USER, MANAGER, ADMIN = ('ordinary_user', 'manager', 'admin')
+VIA_EMAIL, VIA_PHONE = ('via_email', 'via_phone')
+NEW, CODE_VERIFIED, DONE, PHOTO_DONE = ('new', 'code_verified', 'done', 'photo_done')
+
+
+class CustomUser(BaseModel, AbstractUser):
+    USER_ROLE = (
+        (ORDINARY_USER, ORDINARY_USER),
+        (MANAGER, MANAGER),
+        (ADMIN, ADMIN)
+    )
+    AUTH_TYPE = (
+        (VIA_EMAIL, VIA_EMAIL),
+        (VIA_PHONE, VIA_PHONE)
+    )
+    AUTH_STATUS = (
+        (NEW, NEW),
+        (CODE_VERIFIED, CODE_VERIFIED),
+        (DONE, DONE),
+        (PHOTO_DONE, PHOTO_DONE)
+    )
+
+    auth_type = models.CharField(max_length=31, choices=AUTH_TYPE)
+    user_role = models.CharField(max_length=31, choices=USER_ROLE, default=ORDINARY_USER)
+    auth_status = models.CharField(max_length=31, choices=AUTH_STATUS, default=NEW)
+    email = models.EmailField(unique=True, blank=True, null=True)
+    phone_number = models.CharField(max_length=13, unique=True, blank=True, null=True)
+    photo = models.ImageField(upload_to='users_photos/', blank=True, null=True, validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png'])])
+
+    def __str__(self):
+        return self.username
+
+    def create_verify_code(self, verify_type):
+        code = "".join([str(random.randint(0, 10000) % 10) for _ in range(4)])
+        CodeVerified.objects.create(
+            user_id=self.id,
+            verify_type=verify_type,
+            code=code
+        )
+        return code
+
+    def check_username(self):
+        if not self.username:
+            temp_username = f'instagram-{uuid.uuid4().__str__().split("-")[-1]}'
+            while CustomUser.objects.filter(username=temp_username):
+                temp_username = f"{temp_username}{random.randint(0, 9)}"
+            self.username = temp_username
+
+    def check_email(self):
+        if self.email:
+            normalize_email = self.email.lower()
+            self.email = normalize_email
+
+    def check_pass(self):
+        if not self.password:
+            temp_password = f'password-{uuid.uuid4().__str__().split("-")[-1]}'
+            self.password = temp_password
+
+    def hashing_password(self):
+        if not self.password.startswith('pbkdf2_sha256'):
+            self.set_password(self.password)
+
+    def token(self):
+        refresh = RefreshToken.for_user(self)
+        return {
+            "access": str(refresh.access_token),
+            "refresh_token": str(refresh)
+        }
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super(CustomUser, self).save(*args, **kwargs)
+
+    def clean(self):
+        self.check_email()
+        self.check_username()
+        self.check_pass()
+        self.hashing_password()
+
+
+EXPIRATION_PHONE = 2
+EXPIRATION_EMAIL = 5
+
+
+class CodeVerified(BaseModel):
+    AUTH_TYPE = (
+        (VIA_EMAIL, VIA_EMAIL),
+        (VIA_PHONE, VIA_PHONE)
+    )
+    code = models.CharField(max_length=4, blank=True, null=True)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='verify_codes')
+    verify_type = models.CharField(max_length=31, choices=AUTH_TYPE)
+    expiration_time = models.DateTimeField()
+    code_status = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        if self.verify_type == VIA_EMAIL:
+            self.expiration_time = datetime.now() + timedelta(minutes=EXPIRATION_EMAIL)
+        else:
+            self.expiration_time = datetime.now() + timedelta(minutes=EXPIRATION_PHONE)
+        super(CodeVerified, self).save(*args, **kwargs)
+
---
+++ users/serializers.py
@@ -0,0 +1,85 @@
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
+from shared.utility import chech_email_or_phone_number
+from .models import CustomUser, VIA_EMAIL, VIA_PHONE
+
+
+class SignUpSerializer(serializers.ModelSerializer):
+    id = serializers.UUIDField(read_only=True)
+    auth_type = serializers.CharField(required=False, read_only=True)
+    auth_status = serializers.CharField(required=False, read_only=True)
+
+    def __init__(self, *args, **kwargs):
+        super(SignUpSerializer, self).__init__(*args, **kwargs)
+        self.fields['email_phone_number'] = serializers.CharField(required=False)
+
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'auth_type', 'auth_status']
+
+
+    def create(self, validated_data):
+        user = super(SignUpSerializer, self).create(validated_data)
+        if user.auth_type == VIA_EMAIL:
+            code = user.create_verify_code(VIA_EMAIL)
+            # send_email(user.email, code)
+        elif user.auth_type == VIA_PHONE:
+            code = user.create_verify_code(VIA_PHONE)
+            # send_email(user.phone_number, code)
+            # send_phone_code(user.phone_number, code)
+        user.save()
+        return user
+
+    def validate(self, data):
+        super(SignUpSerializer, self).validate(data)
+        data = self.auth_validate(data)
+        return data
+
+    @staticmethod
+    def auth_validate(data):
+        print(data)
+        user_input = str(data.get('email_phone_number')).lower()
+        input_type = chech_email_or_phone_number(user_input)
+        print(input_type)
+        if input_type == "email":
+            data = {
+                "email": user_input,
+                "auth_type": VIA_EMAIL
+            }
+        elif input_type == "phone":
+            data = {
+                "phone_number": user_input,
+                "auth_type": VIA_PHONE
+            }
+        else:
+            data = {
+                'success': False,
+                'message': "To'g'ri telefon raqam yoki email kiriting"
+            }
+            raise ValidationError(data)
+
+        return data
+
+    def validate_email_phone_number(self, value):
+        value = value.lower()
+        if value and CustomUser.objects.filter(email=value).exists():
+            data = {
+                "success": False,
+                "message": "Bu email allaqachon ma'lumotlar bazasida bor"
+            }
+            raise ValidationError(data)
+        elif value and CustomUser.objects.filter(phone_number=value).exists():
+            data = {
+                "success": False,
+                "message": "Bu telefon raqami allaqachon ma'lumotlar bazasida bor"
+            }
+            raise ValidationError(data)
+
+        return value
+
+    def to_representation(self, instance):
+        data = super(SignUpSerializer, self).to_representation(instance)
+        data.update(instance.token())
+        return data
+
